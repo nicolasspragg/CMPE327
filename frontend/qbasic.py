3832f10bc27ebc66de6e2d1977c651181a0fbb33
Nicolas <nicolasspragg@gmail.com>
2017-10-16 16:02:13 -0400

    fix

--- frontend/qbasic.py
+++ frontend/qbasic.py
@@ -1,30 +1,34 @@
 from Console import Console
 from Validity import Validity
+from Session import Session
 import sys, os
 
-def main():
+def qbasic():
 	userInput = Console()
 	tester = Validity()
-
-	accountNum = userInput.accountNumberInput()
-	print(accountNum)
-	tester.checkAccountNumber(accountNum) 
+	newSession = Session()
 
 	cmd = userInput.commandInput()
-	print(cmd)
-	tester.checkCommand(cmd,tester.validCommandsList)
-#broken
-	amt = userInput.amountIn()
-	print(amt)
-	tester.checkAmount(amt,True)
-
-	accName = userInput.accountNameInput()
-	print(accName)
-	tester.checkAccountName(accName)
 
 
-accountsListFile = sys.argv[1]
-accountsList = open(accountsListFile, 'r').readlines()
-accountsList = map(int, accountsList)
-print accountsList
-main()
+	if(cmd == "login"):
+		accType = userInput.accountTypeInput() 
+		newSession.login(accType)
+	elif(cmd == "logout"):
+		
+
+
+
+
+
+	
+
+
+
+
+	
+
+
+
+
+qbasic()
