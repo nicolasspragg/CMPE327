3d1d9988385c7785b7aaed574f77d99d71ad413d
Nicolas <nicolasspragg@gmail.com>
2017-10-31 22:12:29 -0400

    arg passing updated

--- name.sh
+++ name.sh
@@ -1,10 +1,14 @@
-testType= "$1"
 
+array=("$@")
 
-cd tests/testType
+for i in "${array[@]}"
+do
+echo "$i"
+cd tests/$i
 
 find . -name "accounts" -prune -o -type f -name "test_*" | while read line; do 
 	echo "${line#*test_}"
+
 done
-
-
+cd ..
+done
