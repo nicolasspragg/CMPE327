c15ef5bfb2c7a64bcac15eb74d50e7fef46a046b
Nicolas <nicolasspragg@gmail.com>
2017-10-31 22:23:09 -0400

    working

--- name.sh
+++ name.sh
@@ -3,12 +3,10 @@ array=("$@")
 
 for i in "${array[@]}"
 do
-echo "$i"
 cd tests/$i
-
 find . -name "accounts" -prune -o -type f -name "test_*" | while read line; do 
 	echo "${line#*test_}"
-
 done
+cd ..
 cd ..
 done
